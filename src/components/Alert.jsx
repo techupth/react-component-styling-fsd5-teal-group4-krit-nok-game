e1bcb5ac18160f56c65c2fde7b886956863fd1ac
Krit <titatit.akkh@gmail.com>
2023-12-20 15:37:33 +0700

    feat: finish ex component-styling

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,132 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import frown from './icon/frown.svg'
+import alert from './icon/alert-triangle.svg'
+import circle from './icon/alert-circle.svg'
+import check from './icon/check-circle.svg'
 
-function Alert() {}
+function Alert(props) {
+    if(props.type === "error"){
+        return(
+            <div css={css`
+                font: Kanit;
+                font-weight: 700;
+                color: #444444;
+                font-size: 20px;
+                line-height: 29.9px;
+
+                width: 650px;
+                height: 76px;
+                background-color: #F9C8C8;
+                display: flex;
+                align-item: center;
+                padding-top: 10px;
+                padding-left: 30px;
+                border-radius: 10px;
+                margin-top: 20px;
+
+
+            `}>
+                <img src={frown} css={css`
+                    padding-top: 22px;
+                    padding-right: 10px;
+                    width: 24px;
+                    height: 24px;
+                `} />
+                <p>{props.text}</p>
+            </div>
+        )
+    } else if(props.type === "warning"){
+        return(
+            <div css={css`
+                font: Kanit;
+                font-weight: 700;
+                color: #444444;
+                font-size: 20px;
+                line-height: 29.9px;
+
+                width: 650px;
+                height: 76px;
+                background-color: #F9D9C8;
+                display: flex;
+                align-item: center;
+                padding-top: 10px;
+                padding-left: 30px;
+                border-radius: 10px;
+                margin-top: 20px;
+            `}>
+                <img src={alert} css={css`
+                    padding-top: 22px;
+                    padding-right: 10px;
+                    width: 24px;
+                    height: 24px;
+                `} />
+                <p>{props.text}</p>
+            </div>
+        )
+    } else if(props.type === "info"){
+        return(
+            <div css={css`
+                font: Kanit;
+                font-weight: 700;
+                color: #444444;
+                font-size: 20px;
+                line-height: 29.9px;
+
+                width: 650px;
+                height: 76px;
+                background-color: #F9EBC8;
+                display: flex;
+                align-item: center;
+                padding-top: 10px;
+                padding-left: 30px;
+                border-radius: 10px;
+                margin-top: 20px;
+            `}>
+                <img src={circle} css={css`
+                    padding-top: 22px;
+                    padding-right: 10px;
+                    width: 24px;
+                    height: 24px;
+                `} />
+                <p>{props.text}</p>
+            </div>
+        )
+    } else if(props.type === "success"){
+        return(
+            <div css={css`
+                font: Kanit;
+                font-weight: 700;
+                color: #444444;
+                font-size: 20px;
+                line-height: 29.9px;
+
+                width: 650px;
+                height: 76px;
+                background-color: #CEF7CD;
+                display: flex;
+                align-item: center;
+                padding-top: 10px;
+                padding-left: 30px;
+                border-radius: 10px;
+                margin-top: 20px;
+            `}>
+                <img src={check} css={css`
+                    padding-top: 22px;
+                    padding-right: 10px;
+                    width: 24px;
+                    height: 24px;
+                `} />
+                <p>{props.text}</p>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <p>{props.text}</p>
+        </div>
+    )
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,41 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props) {
+    if(props.type === "primary"){
+        return(
+            <button css={css`
+                font: Sarabun;
+                font-size: 8px;
+                font-weight: 400;
+                border-radius: 4px;
+                background-color: #074EE8;
+                width: 171.19px;
+                height: 50px;
+        `}>
+            <h2>{props.text}</h2>
+        </button>
+        );
+    } else if (props.type === "secondary") {
+        return (
+            <button css={css`
+                font: Sarabun;
+                font-size: 8px;
+                font-weight: 400;
+                border-radius: 4px;
+                background-color: #07A4E8;
+                width: 171.19px;
+                height: 50px;
+                margin-top: 20px;
+        `}>
+            <h2>{props.text}</h2>
+        </button>
+        );
+    }
+    
+    return (
+        <button>{props.text}</button>
+    )
+}
+
+export default Button;
