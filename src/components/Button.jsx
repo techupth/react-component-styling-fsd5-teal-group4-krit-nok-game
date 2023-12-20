e6726de9390e8e703f57eecbea7d22050447254c
Nok <siriwan.skk@gmail.com>
2023-12-21 01:13:28 +0700

    feat: finish ex component-styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,53 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  // const primary = "#074EE8";
+  // const secondary = "#07A4E8";
+
+  if (props.type === "primary") {
+    return (
+      <button
+        css={css`
+          display: flex;
+          padding: 0px 16px;
+          margin: 10px;
+          justify-content: center;
+          align-items: center;
+          width: 171.19px;
+          height: 50px;
+          background-color: #074ee8;
+          font-family: Sarabun;
+          font-size: 16px;
+          font-weight: 400;
+          border-radius: 4px;
+          color: white;
+        `}>
+        <h2>{props.text}</h2>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        css={css`
+          display: flex;
+          padding: 0px 16px;
+          margin: 10px;
+          justify-content: center;
+          align-items: center;
+          width: 171.19px;
+          height: 50px;
+          background-color: #07a4e8;
+          font-family: Sarabun;
+          font-size: 16px;
+          font-weight: 400;
+          border-radius: 4px;
+          color: white;
+        `}>
+        <h2>{props.text}</h2>
+      </button>
+    );
+  }
+  return <button>{props.type}</button>;
+}
